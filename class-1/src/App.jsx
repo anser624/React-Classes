c6016f932c9cea7da25512089daeba184141f997
Anser <am24626526jan@gmail.com>
2025-03-09 00:17:08 +0500

    added about section in app file

--- class-1/src/App.jsx
+++ class-1/src/App.jsx
@@ -3,6 +3,7 @@ import reactLogo from "./assets/react.svg";
 import viteLogo from "/vite.svg";
 import "./App.css";
 import Navbar from "./compnents/Navbar";
+import About from "./compnents/About";
 
 function App() {
   const [count, setCount] = useState(0);
@@ -31,6 +32,7 @@ function App() {
           Click on the Vite and React logos to learn more
         </p>
       </div>
+      <About/>
     </>
   );
 }
---
+++ class-1/src/compnents/About.jsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+function About() {
+  return (
+    <>
+      <div className="container text-center my-2 w-100  p-3 bg-light">
+        <p className="text-center my-2 fs-6 w-100 bg-primary p-3 text-white border rounded-5"> 
+          Welcome to our platform! We are dedicated to providing you with the
+          best tools and resources to enhance your skills and achieve your
+          goals. Whether you're a beginner or an experienced developer, our
+          comprehensive guides and tutorials will help you stay ahead in the
+          ever-evolving world of technology. Join our community today and take
+          the first step towards mastering new skills, collaborating with
+          like-minded individuals, and building innovative projects. Let's grow
+          together!
+        </p>
+      </div>
+    </>
+  );
+}
+export default About;
